f9c641f9d53d185b499c974fefd5901b150bcb55
Kate Lynner <113389068+klynner33@users.noreply.github.com>
2022-12-19 12:31:57 -0600

    Added removed playerSelection function and replaced with playRound function

--- external.js
+++ external.js
@@ -1,6 +1,7 @@
+
 function getComputerChoice() {
    let getComputerChoice = Math.random();
-    if (getComputerChoice < 0.34) {
+    if (getComputerChoice <= 0.34) {
         return "rock";
     } else if (getComputerChoice <= 0.67) {
         return "paper";
@@ -9,15 +10,38 @@ function getComputerChoice() {
     } 
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
 
-function playerSelection(myPick) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "paper" && computerSelection === "rock"  ||
+        playerSelection === "scissors" && computerSelection === "paper") {
+            return "You win!";
+        } else {
+            return "Computer wins. Try again!";
+        }
+}
+
+const playerSelection = "PAPER".toLowerCase();
+const computerSelection = getComputerChoice();
+
+console.log (playRound(playerSelection, computerSelection));
+console.log("Computer: " + computerSelection);
+console.log("Player: " + playerSelection);
+
+/* function getPlayerChoice(myPick) {
     if (myPick.toLowerCase() === "rock") {
         return "rock";
-    } else if (myPick.toLowerCase === "paper") {
+    } else if (myPick.toLowerCase() === "paper") {
         return "paper";
     }   else {
         return "scissors";
     }
 
     }
+
+    console.log(getPlayerChoice("rock")); */
+
+
