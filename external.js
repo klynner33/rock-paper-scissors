0a4be9d6c5f123a31bedbe07982a96c9934362a6
Kate Lynner <113389068+klynner33@users.noreply.github.com>
2023-02-25 10:02:30 -0600

    Reworked functions from console to interacting with webpage

--- external.js
+++ external.js
@@ -1,3 +1,38 @@
+const selectionRock = document.querySelector("#rock")
+const selectionPaper = document.querySelector("#paper")
+const selectionScissors = document.querySelector("#scissors")
+const computerChoiceBox = document.querySelector("#computerChoiceBox")
+const playerScorebox = document.querySelector(".scorebox.player")
+const computerScorebox = document.querySelector(".scorebox.computer")
+const result = document.querySelector(".result")
+const playBtn = document.querySelector("#playBtn")
+
+let playerSelection;
+let computerSelection = "";
+let playerScore = 0;
+let computerScore = 0;
+
+selectionRock.addEventListener("click", function(e) {
+    getPlayerChoice("rock");
+});
+selectionPaper.addEventListener("click", function(e) {
+    getPlayerChoice("paper");
+});
+selectionScissors.addEventListener("click", function(e) {
+    getPlayerChoice("scissors");
+});
+
+playBtn.addEventListener("click", function(e) { 
+    location.reload();
+});
+
+function getPlayerChoice(playerChoice) {
+    playerSelection = playerChoice;
+    computerSelection = getComputerChoice();
+    convertComputerChoice();
+    playRound(playerSelection, computerSelection);
+    checkScore();
+}
 
 function getComputerChoice() {
    let getComputerChoice = Math.random();
@@ -6,74 +41,55 @@ function getComputerChoice() {
     } else if (getComputerChoice <= 0.67) {
         return "paper";
     } else {
-        return "scissors";
+        return "scissors"; 
     } 
 }
 
-let playerScore = 0;
-let computerScore = 0;
+function convertComputerChoice() {
+    if (computerSelection === "rock") {
+        computerChoiceBox.textContent = "✊";
+    } else if (computerSelection === "paper") {
+        computerChoiceBox.textContent = "✋";
+    } else if (computerSelection === "scissors") {
+        computerChoiceBox.textContent = "✌";
+    }
+}
 
 function playRound(playerSelection, computerSelection) {
-    
-    if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    } else if (
+   
+        if (playerSelection === computerSelection) {
+            result.textContent = "It's a tie!";
+            
+            playerScorebox.textContent = playerScore;
+            computerScorebox.textContent = computerScore;
+        } else if (
         playerSelection === "rock" && computerSelection === "scissors" ||
         playerSelection === "paper" && computerSelection === "rock"  ||
         playerSelection === "scissors" && computerSelection === "paper") {
-             playerScore++
-            return "You win!";
+            result.textContent = "You win this round!";
+            playerScore++;
+            playerScorebox.textContent = playerScore;
+            computerScorebox.textContent = computerScore;
         } else if (
         playerSelection === "scissors" && computerSelection === "rock" ||
         playerSelection === "rock" && computerSelection === "paper"  ||
         playerSelection === "paper" && computerSelection === "scissors") {
-             computerScore++
-            return "Computer wins. Try again!";
-        } else {
-            return "That is not a valid answer. Try again!"
-        }
-    }
-
- function game() {
-    function playRound() {
-    for (let i = 0; i < 5; i++) {
-        if (i === "You win!") {
-            playerScore++;
-        } else if (i === "Computer wins. Try again!") {
+            result.textContent = "Computer wins this round.";
             computerScore++;
+            playerScorebox.textContent = playerScore;
+            computerScorebox.textContent = computerScore;
+        } else {
+            result.textContent = "Error. Try again!";
+            playerScorebox.textContent = playerScore;
+            computerScorebox.textContent = computerScore;
         }
+}
+
+function checkScore() {
+    if (playerScore === 5) {
+        result.textContent = "YOU WON THE GAME!!! Play again!"
+    } else if(computerScore === 5) {
+        result.textContent = "GAME OVER. Try again!"
     }
 }
-}
-let choice = prompt("What do you choose?");
-const playerSelection = choice.toLowerCase();
-const computerSelection = getComputerChoice();
-console.log("Computer picks: " + computerSelection);
-console.log("Player picks: " + playerSelection);
-console.log (playRound(playerSelection, computerSelection));
-console.log(playerScore);
-console.log(computerScore);
-
-
-
-
-/* function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let result = playRound(playerSelection, computerSelection);
-        if (result === "You win!") {
-            playerScore++;
-        } else if (result === "Computer wins. Try again!") {
-            computerScore++;
-        } else {
-            return "No points for a tie!";
-        }   
-    }
-    console.log("Player score: " + playerScore);
-    console.log("Computer score: " + computerScore); 
-} */
-
-game();
-
 
